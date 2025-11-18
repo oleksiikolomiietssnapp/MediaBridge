553593566b523b0b5e024b218b95c897d8d5777d
Oleksii Kolomiiets <oleksii@snapp.team>
2025-11-18 10:31:49 +0100

    add example app

---
+++ Example/Example/Example/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Oleksii Kolomiiets on 11/16/25.
+//
+
+import SwiftUI
+import MediaBridge
+import MediaPlayer
+
+struct ContentView: View {
+    @Environment(\.library) var library
+    @State var songs: [MPMediaItem] = []
+    var body: some View {
+        Text(songs.count, format: .number)
+            .task {
+                do {
+                    let songs = try await library.fetchSongs()
+                    print("Fetched: ", songs.count, "songs.")
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/Example/Example/ExampleApp.swift
@@ -0,0 +1,22 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Oleksii Kolomiiets on 11/16/25.
+//
+
+import MediaBridge
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+extension EnvironmentValues {
+    @Entry var library: MusicLibrary = MusicLibrary()
+}
